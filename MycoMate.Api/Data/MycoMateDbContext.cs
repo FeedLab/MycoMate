877fa930fc5370e414f5cb6e95e99339526e0de1
Jonny Nilsson <jompa67@gmail.com>
2026-06-26 08:33:33 +0700

    Initial commit

---
+++ MycoMate.Api/Data/MycoMateDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MycoMate.Api.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<IdentityUser>(options)
+{
+}
